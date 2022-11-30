628210b55c7fb5ff55321cb43a97a5a507007e5f
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-30 11:36:57 -0800

    Create Empty component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./styles.scss";
 
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
   return (
