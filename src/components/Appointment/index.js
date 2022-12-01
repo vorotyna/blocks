c6e278a61a926c7948211f1c29c818ffd59bd719
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-01 09:41:39 -0800

    Update Application component to render mock interview schedule

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,7 +5,6 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 
-
 export default function Appointment(props) {
   return (
     <article className="appointment">
