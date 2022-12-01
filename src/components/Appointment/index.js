35da1b3d0b8c6286a59b00d901d5d9ab455ae389
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-01 09:08:05 -0800

    Update Appointment component and stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { Fragment } from 'react';
 
 import "./styles.scss";
-
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
-import Confirm from "components/Appointment/Confirm";
-import Status from "components/Appointment/Status";
-import Error from "components/Appointment/Error";
-import Form from "components/Appointment/Form";
+
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ?
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+        :
+        <Empty />
+      }
+    </article >
   );
 }
