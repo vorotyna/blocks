75ab26db4de1c1ff5acf7505a41b3f4d4b581281
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-02 09:24:21 -0800

    Create helper function getAppointmentsForDays

---
+++ src/helpers/selectors.js
@@ -0,0 +1,16 @@
+export function getAppointmentsForDay(state, dayOfWeek) {
+  let appointmentIDs = [];
+  for (let day of state.days) {
+    if (day.name === dayOfWeek) {
+      appointmentIDs = day.appointments;
+    }
+  };
+  if (appointmentIDs.length === 0) {
+    return [];
+  };
+  let daysAppointments = [];
+  for (let appointment of appointmentIDs) {
+    daysAppointments.push(state.appointments[appointment]);
+  };
+  return daysAppointments;
+};
