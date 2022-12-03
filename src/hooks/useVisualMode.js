a1e57b52c370c51a8e046593b33e6a1ada7618af
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-02 16:03:23 -0800

    Build custom hook to update mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, -1)]);
+    }
+    setHistory(prev => [...prev, newMode]);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+}
