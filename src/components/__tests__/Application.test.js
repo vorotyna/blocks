bc74f04006c1505eeb2cc2ceeaa8024743320fcc
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-06 19:25:18 -0800

    Refactor test to use async await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -13,19 +13,18 @@ import { render, waitForElement, fireEvent } from "@testing-library/react";
   We import the component that we are testing
 */
 import Application from "components/Application";
-import Appointment from "components/Appointment";
 
 
 
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+it("changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday"))
-    .then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
 
 
